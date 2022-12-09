a55ecc6b49d4f7749d6ef145bb7e0364be4882a6
jaewon <jaewoniik94@gmail.com>
2022-12-09 21:25:18 +0900

    Feat : 팔로우 기능 추가
    - 팔로우 등록/취소 기능 추가
    - 팔로잉/팔로워 목록 조회 대상을 해당 프로필 유저로 변경
    Open #10

--- static/js/api.js
+++ static/js/api.js
@@ -63,9 +63,28 @@ async function getFoodList() {
   return response_json;
 }
 
+// 팔로우 등록/취소
+async function DoFollow(user_id) {
+  const response = await fetch(`${backend_base_url}/users/follow/${user_id}/`, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("access"),
+    },
+    method: "POST",
+  });
+  response_json = await response.json();
+
+  if (response.status == 200) {
+    alert(response_json["message"]);
+    window.location.replace(`${frontend_base_url}/profile.html?id=${user_id}`);
+  } else {
+    alert(response.status);
+  }
+}
+
 // 팔로잉/팔로워 리스트 가져오기
-async function getFollowList() {
-  const response = await fetch(`${backend_base_url}/users/follow/`, {
+async function getFollowList(user_id) {
+  const response = await fetch(`${backend_base_url}/users/follow/${user_id}`, {
     headers: {
       Authorization: "Bearer " + localStorage.getItem("access"),
     },
--- static/js/profile.js
+++ static/js/profile.js
@@ -36,12 +36,22 @@ async function Profile(user_id) {
     } else {
         gender.innerText = "성별 : 사용 안 함"
     }
+
+    const do_follow = document.getElementById("do_follow");
+    
+    const do_follow_button = document.createElement("button");
+    do_follow_button.innerText = "팔로우";
+    do_follow_button.setAttribute("id", user_id);
+    do_follow_button.setAttribute("class", "btn btn-warning");
+    do_follow_button.setAttribute("onclick", "DoFollow(this.id)");
+    do_follow.appendChild(do_follow_button);
 }
 Profile(user_id)
+    
 
 // 팔로잉/팔로워 리스트 가져오기
-async function FollowList() {
-    follows = await getFollowList();
+async function FollowList(user_id) {
+    follows = await getFollowList(user_id);
 
     follows.following.forEach((following) => {
         const following_list = document.getElementById("following-modal-body");
@@ -60,43 +70,43 @@ async function FollowList() {
         follower_list.appendChild(newFollower);
     });
 }
-FollowList()
+FollowList(user_id)
 
 // 해당 프로필 유저가 좋아요한 메뉴 리스트 가져오기
-async function MyFoodList() {
-    foods = await getMyFoodList();
+// async function MyFoodList() {
+//     foods = await getMyFoodList();
 
-    const card_list = document.getElementById("card-list");
+//     const card_list = document.getElementById("card-list");
 
-    foods.forEach((food) => {
-        const newCard = document.createElement("li");
-        newCard.setAttribute("class", "card");
-        newCard.setAttribute("id", "card");
+//     foods.forEach((food) => {
+//         const newCard = document.createElement("li");
+//         newCard.setAttribute("class", "card");
+//         newCard.setAttribute("id", "card");
 
-        const newImg = document.createElement("a");
-        newImg.setAttribute("class", "card-image");
-        newImg.src = `https://storage.googleapis.com/jjalbot/2018/12/IPJVU9tjx/zzal.jpg`;
-        newImg.setAttribute("style", newImg.src)
-        newImg.setAttribute("data-image-full", newImg.src)
+//         const newImg = document.createElement("a");
+//         newImg.setAttribute("class", "card-image");
+//         newImg.src = `https://storage.googleapis.com/jjalbot/2018/12/IPJVU9tjx/zzal.jpg`;
+//         newImg.setAttribute("style", newImg.src)
+//         newImg.setAttribute("data-image-full", newImg.src)
 
-        const image = document.createElement("img");
-        image.src = `https://storage.googleapis.com/jjalbot/2018/12/IPJVU9tjx/zzal.jpg`;
-        image.setAttribute("alt", "Psychopomp")
-        newImg.appendChild(image)
-        newCard.appendChild(newImg)
+//         const image = document.createElement("img");
+//         image.src = `https://storage.googleapis.com/jjalbot/2018/12/IPJVU9tjx/zzal.jpg`;
+//         image.setAttribute("alt", "Psychopomp")
+//         newImg.appendChild(image)
+//         newCard.appendChild(newImg)
 
-        const newDescription = document.createElement("a");
-        newDescription.setAttribute("class", "card-description");
-        newCard.appendChild(newDescription)
+//         const newDescription = document.createElement("a");
+//         newDescription.setAttribute("class", "card-description");
+//         newCard.appendChild(newDescription)
 
-        const newMenu = document.createElement("h2");
-        const newCategory = document.createElement("p");
-        newMenu.innerText = food.menu;
-        newCategory.innerText = food.major_category;
-        newDescription.appendChild(newMenu)
-        newDescription.appendChild(newCategory)
+//         const newMenu = document.createElement("h2");
+//         const newCategory = document.createElement("p");
+//         newMenu.innerText = food.menu;
+//         newCategory.innerText = food.major_category;
+//         newDescription.appendChild(newMenu)
+//         newDescription.appendChild(newCategory)
 
-        card_list.appendChild(newCard)
-    });
-}
-MyFoodList()
+//         card_list.appendChild(newCard)
+//     });
+// }
+// MyFoodList()
