3e9795da324ed4bf95e188bf0b7d9e7b78bb62f1
choding91 <whdlsrjf91@naver.com>
2022-12-13 19:08:03 +0900

    Feat : 커뮤니티 게시글 검색 기능 추가
    - 이름, 제목, 내용 범위 내에서 등록된 게시글 검색 가능
    Close #24

--- static/js/community.js
+++ static/js/community.js
@@ -1,3 +1,7 @@
+if (!token) {
+  window.location.replace(`${frontend_base_url}/login.html`);
+}
+
 // 전체 페이지 로딩이 완료될 때까지 기다림====================================================221207 이태은
 window.addEventListener("load", function () {
   // 실제 페이지 로드의 지연을 시뮬레이트하기 위한 setTimeout
