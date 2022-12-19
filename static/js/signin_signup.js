c8073e2fc2fc66b3c9483fc81efe3b36bba190e9
choding91 <whdlsrjf91@naver.com>
2022-12-19 12:12:47 +0900

    Fix : 로그인/회원가입 경로 수정
    - 백엔드 서버 배포 기준으로 경로 재설정

--- static/js/signin_signup.js
+++ static/js/signin_signup.js
@@ -1,6 +1,6 @@
 if (token) {
-    window.location.replace(`${frontend_base_url}/main.html`);
-  }
+  window.location.replace(`${frontend_base_url}/main.html`);
+}
 
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
@@ -10,65 +10,71 @@ signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
-
-
 
 // 입력 없으면 표시 현재 구현 안됨
 window.onload = () => {
-    console.log("load")
-}
+  console.log("load");
+};
 
-window.addEventListener('load', () => {
-    const forms = document.getElementsByClassName('validation-form');
+window.addEventListener(
+  "load",
+  () => {
+    const forms = document.getElementsByClassName("validation-form");
 
     Array.prototype.filter.call(forms, (form) => {
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
 
-            form.classList.add('was-validated');
-        }, false);
+          form.classList.add("was-validated");
+        },
+        false
+      );
     });
-}, false);
-
+  },
+  false
+);
 
 async function handleSignIn() {
-    const email = document.getElementById("email2").value
-    const password = document.getElementById("password2").value
-    const response = await fetch('http://127.0.0.1:8000/users/api/token/', {
-        headers: {
-            'content-type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        })
-    })
+  const email = document.getElementById("email2").value;
+  const password = document.getElementById("password2").value;
+  const response = await fetch(`${backend_base_url}/users/api/token/`, {
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
 
-    const response_json = await response.json()
+  const response_json = await response.json();
 
-    localStorage.setItem("access", response_json.access);
-    localStorage.setItem("refresh", response_json.refresh);
-    console.log(response_json)
-    const base64Url = response_json.access.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  localStorage.setItem("access", response_json.access);
+  localStorage.setItem("refresh", response_json.refresh);
+  console.log(response_json);
+  const base64Url = response_json.access.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-    }).join(''));
-
-    localStorage.setItem("payload", jsonPayload);
-    window.location.href = 'main.html'
+  localStorage.setItem("payload", jsonPayload);
+  window.location.href = "main.html";
 }
-
-
 
 // async function handleMock() {
 //     // const response = await fetch('http://127.0.0.1:8000/users/mock/', {
@@ -100,7 +106,7 @@ async function handleSignIn() {
 //     let code = new URL(window.location.href).searchParams.get('code')
 //     if (code) {
 //         const response = await fetch('http://127.0.0.1:8000/users/google/callback/', {
-            
+
 //             headers: {
 //                 'content-type': 'application/json',
 //             },
@@ -129,58 +135,60 @@ async function handleSignIn() {
 //     handleGoogle()
 // }
 
-
 async function handleKakao() {
-    let code = new URL(window.location.href).searchParams.get('code')
-    if (code) {
-        const response = await fetch('http://127.0.0.1:8000/users/kakao/callback/', {
-            
-            headers: {
-                'content-type': 'application/json',
-            },
-            method: "POST",
-            body: JSON.stringify({
-                "code": code
-            })
+  let code = new URL(window.location.href).searchParams.get("code");
+  if (code) {
+    const response = await fetch(`${backend_base_url}/users/kakao/callback/`, {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        code: code,
+      }),
+    });
+    const response_json = await response.json();
+
+    localStorage.setItem("access", response_json.access_token);
+    localStorage.setItem("refresh", response_json.refresh_token);
+
+    const base64Url = response_json.access_token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
         })
-        const response_json = await response.json()
+        .join("")
+    );
 
-        localStorage.setItem("access", response_json.access_token);
-        localStorage.setItem("refresh", response_json.refresh_token);
-
-        const base64Url = response_json.access_token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-
-        }).join(''));
-
-        localStorage.setItem("payload", jsonPayload);
-        window.location.href = 'main.html'
-        }
-    }
-window.onload  = function(){
-    handleKakao()
+    localStorage.setItem("payload", jsonPayload);
+    window.location.href = "main.html";
+  }
 }
+window.onload = function () {
+  handleKakao();
+};
 
-
-
-//---------------------------------------------------------------------------------------------//
-//---------------------------------------------------------------------------------------------//
 //signUp//
-// 입력 없으면 표시 
+// 입력 없으면 표시
 
 async function check_value() {
-    const forms = document.getElementsByClassName('validation-form');
-    Array.prototype.filter.call(forms, (form) => {
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
+  const forms = document.getElementsByClassName("validation-form");
+  Array.prototype.filter.call(forms, (form) => {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
 }
 // window.addEventListener('load', () => {
 //     const forms = document.getElementsByClassName('validation-form');
@@ -196,33 +204,29 @@ async function check_value() {
 // }, false);
 
 async function handleSignUp() {
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    const password_check = document.getElementById("password_check").value
-    const nickname = document.getElementById("nickname").value
-        // const response = await fetch('http://127.0.0.1:8000/users/signup/', {
-    const response = await fetch('http://127.0.0.1:8000/users/signup/', {
-        headers: {
-            'content-type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            "email" : email,
-            "password" : password,
-            "password_check" : password_check,
-            "nickname" : nickname
-
-        })
-    });
-    const response_json = await response.json()
-    console.log(response_json)
-    for(var key in response_json) {
-        console.log("key: ", key)
-        console.log("value: ", response_json[key])
-        console.log("----------------")
-        alert(response_json[key])
-      }
-    
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const password_check = document.getElementById("password_check").value;
+  const nickname = document.getElementById("nickname").value;
+  // const response = await fetch('http://127.0.0.1:8000/users/signup/', {
+  const response = await fetch(`${backend_base_url}/users/signup/`, {
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      password_check: password_check,
+      nickname: nickname,
+    }),
+  });
+  const response_json = await response.json();
+  console.log(response_json);
+  for (var key in response_json) {
+    console.log("key: ", key);
+    console.log("value: ", response_json[key]);
+    console.log("----------------");
+    alert(response_json[key]);
+  }
 }
-
-
